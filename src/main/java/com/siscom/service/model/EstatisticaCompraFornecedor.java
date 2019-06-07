a3fb90930b2af1aba6c6f48d82ad9c7a0fa417ca
cindyjane <cindyjss21@gmail.com>
2019-06-06 21:14:23 -0300

    estatistica

--- src/main/java/com/siscom/service/model/EstatisticaCompraFornecedor.java
+++ src/main/java/com/siscom/service/model/EstatisticaCompraFornecedor.java
@@ -40,7 +40,7 @@ public class EstatisticaCompraFornecedor implements Serializable {
 
     @Override
     public String toString() {
-        return "EstatisticaCompraFornecedor{" +
+        return "Estatistica{" +
                 "nome='" + nome + '\'' +
                 ", vezesVendeu=" + vezesVendeu +
                 ", valorTotal=" + valorTotal +
